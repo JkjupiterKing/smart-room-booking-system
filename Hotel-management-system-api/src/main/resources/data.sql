bb6bd5daa7f01bc7f0e26506ec859d0953e81986
Jupiterking <jupiterkingtechnology@gmail.com>
2025-08-31 17:38:34 +0530

    fixes

---
+++ Hotel-management-system-api/src/main/resources/data.sql
@@ -0,0 +1,4 @@
+DELETE FROM admins;
+--
+--INSERT INTO admins (email, password) VALUES ('admin@example.com', 'admin123');
+INSERT INTO admins (email, password,username) VALUES ('admin@example.com', 'admin123','admin');
